a53c96d18b448cf76df714bd592903d46b255cac
Brian W <brianwalters952@gmail.com>
2021-07-06 19:23:35 -0500

    got seed files working with User model

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static("./public"));
+
 
 app.use(routes);
 
