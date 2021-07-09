a1858ccbfdaa53edf578b893222ac2ec047c4b34
Brian W <brianwalters952@gmail.com>
2021-07-08 22:55:24 -0500

    added logo to tab, and styled sign up page

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -32,11 +32,11 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    alert("You must log out before creating a new profile")
-    res.redirect("/homepage");
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   alert("You must log out before creating a new profile");
+  //   res.redirect("/homepage");
+  //   return;
+  // }
   res.render("signup");
 });
 
